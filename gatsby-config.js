86cd15a98f7c2b018e9fb2c57e1a6c6528d60a94
Res Yeg <shaqk702@gmail.com>
2018-11-09 20:40:41 -0800

    add blogpost

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'My Blog built with GatsbyJS',
+    title: 'Myblog - reactgo',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -25,8 +16,14 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    `gatsby-transformer-remark`,
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
   ],
 }
---
+++ src/templates/blog-post.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Layout from '../components/layout';
+import { graphql } from 'gatsby'
+
+function BlogPost() {
+    return (
+        <Layout>
+            <div>
+                hello post
+        </div>
+        </Layout>
+    )
+}
+
+export default BlogPost
+
+const query = graphql`
+ query PostQuery($slug: String!) {
+     markdownRemark(fields: { slug: { eq: $slug } }) {
+       html
+       frontmatter {
+        title
+        description
+       }
+   }
+`
